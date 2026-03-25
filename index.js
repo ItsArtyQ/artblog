8683fe4566f9d2531f1055faca621ac5a22c01a1
itsartyq <itsartyq@gmail.com>
2026-03-25 14:48:04 +0300

    Update: Add login and register page (Only frontend. Not adaptive ui)

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
 app.listen(port, () => {
   console.log(`Server running: http://127.0.0.1:${port}`);
 });
